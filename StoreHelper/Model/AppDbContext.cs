ec6f02f68db7f90fa2d6f6318df6750cd0d6cd94
Vinogardov Alexander <a3vinogradov@gmail.com>
2024-07-18 18:00:04 +0300

    save

---
+++ StoreHelper/Model/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreHelper.Model
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<Store> Stores { get; set; } = null!;
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb2;Username=postgres;Password=1");
+		}
+	}
+}
