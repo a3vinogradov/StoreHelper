3a4530cfb49954034782cc0333e24b7702f13649
Vinogradov Alexander <a3vinogradov@gmail.com>
2025-05-15 13:58:26 +0300

    Job-002 Добавить хранилища и Вещи

--- StoreHelherAttempt2/Models/Good.cs
+++ StoreHelherAttempt2/Models/Good.cs
@@ -5,9 +5,12 @@ namespace StoreHelherAttempt2.Models
     public class Good
     {
         [Key]
+        [Display(Name = "Идентификатор")]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Наименование")]
         public required string Name { get; set; }
+        [Display(Name = "Описание")]
         public string? Description { get; set; }
     }
 }
--- StoreHelherAttempt2/Models/Purchase.cs
+++ StoreHelherAttempt2/Models/Purchase.cs
@@ -8,19 +8,31 @@ namespace StoreHelherAttempt2.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Входящий номер")]
+
         public required string IncomNumber { get; set; }
         
         [Required]
+        [Display(Name = "Наименование")]
+
         public required string Name { get; set; }
-        
+
+        [Display(Name = "Описание")]
+
         public string? Description { get; set; }
         
         [Required]
+        [Display(Name = "Дата создания")]
+
         public DateTimeOffset DateCreate { get; set; }
-        
+
+        [Display(Name = "Дата последнего изменения")]
+
         public DateTimeOffset? DateUpdate { get; set; }
         
         [Required]
+        [Display(Name = "Склад")]
+
         public required Store Store { get; set; }
     }
 }
